9c888be30e0c572b2ea573e34c0573dddaceb895
sunny <sunny@lianj.com>
2017-05-16 18:38:15 +0800

    提交模仿饿了吗代码

---
+++ sell/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <v-header></v-header>
+    </div>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+  console.log(header);
+  export default{
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style>
+
+</style>
